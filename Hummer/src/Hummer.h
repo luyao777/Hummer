b3c5c9d4abdf5ee2cf04c783e20ded3d0d6f3383
Yao Lu <luyao777@vip.qq.com>
2022-07-31 17:22:03 +0800

    [feat] add Log System and Definations

--- Hummer/src/Hummer.h
+++ Hummer/src/Hummer.h
@@ -2,6 +2,7 @@
 // for use for hummer applications
 #include <stdio.h>
 #include "Hummer/Application.h"
+#include "Hummer/Log.h"
 // ---- Entry Point ----
 #include "Hummer/EntryPoint.h"
 // ---------------------
--- Hummer/src/Hummer/EntryPoint.h
+++ Hummer/src/Hummer/EntryPoint.h
@@ -7,6 +7,11 @@ extern Hummer::Application* Hummer::CreateApplication();
 int main(int argc, char **argv){
     printf("Hummer Engine is runmning!\n");
 
+    Hummer::Log::Init(); 
+    HM_CORE_ERROR("Initialized log!");
+    int a = 5;
+    HM_INFO("Hello! Var={0}",a);
+
     auto app = Hummer::CreateApplication();
     app->Run();
     delete app;
@@ -14,3 +19,4 @@ int main(int argc, char **argv){
     return 0;
 }
 #endif
+
